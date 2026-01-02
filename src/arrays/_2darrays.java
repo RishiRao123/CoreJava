44880c7ae0cf34caeb3c58b80d604f6cca8963ea
RishiRao123 <rishiraopuskuri@gmail.com>
2026-01-02 23:14:59 +0530

    2-d arrays code added

--- src/arrays/_2darrays.java
+++ src/arrays/_2darrays.java
@@ -6,12 +6,30 @@ public class _2darrays {
 
 		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-		System.out.println("2D Array Elements:");
+//		System.out.println("2D Array Elements:");
+//		for (int i = 0; i < matrix.length; i++) {
+//			for (int j = 0; j < matrix[i].length; j++) {
+//				System.out.print(matrix[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+
+		// Row-wise sum
 		for (int i = 0; i < matrix.length; i++) {
+			int rowSum = 0;
 			for (int j = 0; j < matrix[i].length; j++) {
-				System.out.print(matrix[i][j] + " ");
+				rowSum += matrix[i][j];
 			}
-			System.out.println();
+			System.out.println("Sum of row " + i + ": " + rowSum);
+		}
+
+		// Column-wise sum
+		for (int j = 0; j < matrix[0].length; j++) {
+			int colSum = 0;
+			for (int i = 0; i < matrix.length; i++) {
+				colSum += matrix[i][j];
+			}
+			System.out.println("Sum of column " + j + ": " + colSum);
 		}
 	}
 }
