d6cfcf82106c81a868fa5a82fae3c0ad3c7012f9
RishiRao123 <rishiraopuskuri@gmail.com>
2026-01-02 23:16:36 +0530

    Implement transpose of a 2D array

--- src/arrays/_2darrays.java
+++ src/arrays/_2darrays.java
@@ -31,5 +31,26 @@ public class _2darrays {
 			}
 			System.out.println("Sum of column " + j + ": " + colSum);
 		}
+
+//		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+
+		int[][] transpose = new int[cols][rows];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				transpose[j][i] = matrix[i][j];
+			}
+		}
+
+		System.out.println("Transpose of the matrix:");
+		for (int i = 0; i < transpose.length; i++) {
+			for (int j = 0; j < transpose[i].length; j++) {
+				System.out.print(transpose[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
