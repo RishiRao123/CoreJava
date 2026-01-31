8fed926a5c96bea1ae4a62f5321f313bdc130e3f
RishiRao123 <rishiraopuskuri@gmail.com>
2026-02-01 01:34:04 +0530

    Practise code Strings updated

---
+++ src/strings/String1.java
@@ -0,0 +1,18 @@
+package strings;
+
+// 1. String is immutable , StringBuffer is mutable. Mutable means changes can be made. Immutable means 
+// Changes can't be made
+// 
+// Case1 : String str = new String("durga");     StringBuffer sb = new StringBuffer("durga");
+//		   str.concat("software"); 				sb.append("software");
+//         System.out.println(str);				System.out.println(sb);  
+//         result : durga (immutable)			result : durge software (mutable)
+
+public class String1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
